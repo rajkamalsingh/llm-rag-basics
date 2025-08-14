b0fe94af84673c9bb91591c9e3487323df1527de
rajkamalsingh <rajkamalsingh0001@gmail.com>
2025-08-14 16:58:21 -0400

    changes and fixes

--- llm-rag-memory/utils.py
+++ llm-rag-memory/utils.py
@@ -1,8 +1,10 @@
-from langchain.document_loaders import PyPDFLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain.vectorstores import FAISS
-from langchain.embeddings import OpenAIEmbeddings
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_community.vectorstores import FAISS
+from langchain_openai import OpenAIEmbeddings
+import openai, os
 
+openai.api_key = os.getenv("OPENAI_API_KEY", "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 ")
 def process_pdf(file_path):
     loader = PyPDFLoader(file_path)
     pages = loader.load()
@@ -10,7 +12,7 @@ def process_pdf(file_path):
     splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap = 200)
     chunks = splitter.split_documents(pages)
 
-    embeddings = OpenAIEmbeddings()
+    embeddings = OpenAIEmbeddings(openai_api_key="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")
     vectorstore = FAISS.from_documents(chunks,embeddings)
 
     return vectorstore
