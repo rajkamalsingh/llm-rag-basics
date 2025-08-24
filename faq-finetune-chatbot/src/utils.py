e0755bcc2b29618b526e4a3ff92dacff025394e1
rajkamalsingh <rajkamalsingh0001@gmail.com>
2025-08-23 21:51:26 -0400

    fixes

--- faq-finetune-chatbot/src/utils.py
+++ faq-finetune-chatbot/src/utils.py
@@ -6,13 +6,16 @@ def get_tokenizer(model_name="google/flan-t5-small"):
 
 def preprocess(example):
     return {
-        "input_text":"Question: " +example["question"] +"\nAnswer:",
-        "target_text": example["answer"]
+        "input_text":"Question: " +example["query"] +"\nAnswer:",
+        "target_text": "This is a placeholder answer."
     }
 
 def tokenize_function(batch, tokenizer, max_length=128):
+    print(batch["input_text"][:2])
+    print(batch["target_text"][:2])
     inputs = tokenizer(batch["input_text"], padding="max_length", truncation = True, max_length=max_length)
     labels = tokenizer(batch["target_text"], padding="max_length", truncation = True, max_length = max_length)
     inputs["labels"] = labels["input_ids"]
+
     return inputs
 
